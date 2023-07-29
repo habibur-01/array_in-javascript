24386198ab4fa280a30d91815ffabfadb43d1a2d
habibur-01 <h.r.habibur08@gmail.com>
2023-07-29 23:55:11 +0600

    js pop-push

---
+++ array_index.js
@@ -0,0 +1,17 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+// get element value by index
+console.log(numbers[0]);
+
+// can take any index as a variable
+var element = numbers[1];
+console.log(element);
+
+// change or set index value
+numbers[1] = 77;
+numbers[3] = 44;
+console.log(numbers);
+
+// find index of an element
+var positionIndex = numbers.indexOf(89);
+console.log(positionIndex);
---
+++ pusharray.js
@@ -0,0 +1,8 @@
+var numbers = [78, 58, 85, 89]
+// add a element in a array
+numbers.push(63);
+ console.log(numbers);
+
+//  delete an element from array
+numbers.pop();
+console.log(numbers);
